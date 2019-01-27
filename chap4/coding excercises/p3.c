987d9da8efd943a0e432dbb4d184ecbadec57090
A.Rupesh <a.rupesh66@gmail.com>
2019-01-28 03:31:21 +0530

    chap4 till p5.c

--- chap4/coding excercises/p3.c
+++ chap4/coding excercises/p3.c
@@ -1,2 +1,45 @@
 /* If the 3 sides of the triangle are entered through the keyboard WAP to check whether the triangle is valid or not.The triangle is valid if the sum of two sides is greater than the largest of three sides */
 
+#include<stdio.h>
+int main()
+{
+	int side1,side2,side3,largeside,sum;
+	
+	printf("Enter the three sides of the triangle:\n");
+	scanf("%d%d%d",&side1,&side2,&side3);
+	
+	if(side1 > side2)
+		{
+			
+	if(side1 > side3)
+		{
+		 sum = side2 + side3;
+		 largeside = side1;
+		}
+	else
+		
+	  {
+		sum =side1 + side2;
+		largeside = side3;
+	}
+  }
+	else
+{
+	if(side2 > side3)
+	{
+		sum = side1 + side2;
+		largeside = side2;
+	}
+	else
+	 {
+		sum = side1 + side2;
+		largeside = side3;
+	}
+  }
+	
+	if(sum > largeside)
+		printf("The triangle is valid triangle\n");
+	else
+		printf("The triangle is invalid triangle\n");
+	return 0;
+}
---
+++ chap4/coding excercises/p4.c
@@ -0,0 +1,30 @@
+/* If the three sides of the triangle are entered through the keyboard WAP to check whether the triangle is isosceles,equilateral,scalene or right-angled triangle */
+
+#include<stdio.h>
+int main()
+{
+	int s1,s2,s3,a,b,c;
+	
+	printf("Enter three sides of a triangle:\n");
+	scanf("%d%d%d",&s1,&s2,&s3);
+	
+	if(s1 != s2 && s2 != s3 && s3 != s1)
+		printf("Scalene triangle\n");
+	if((s1 == s2) && (s2 != s3))
+		printf("Isosceles triangle\n");
+	if((s2 == s3) && (s3 != s1))
+		printf("Isosceles triangle\n");
+	if((s1 == s3) && (s3 != s2))
+		printf("Isoscelec triangle\n");
+	if((s1 == s2 && s2 == s3))
+		printf("Equilateral triangle\n");
+	
+	a = (s1*s1) == (s2*s2) + (s3*s3);
+	b = (s2*s2) == (s1*s1) + (s3*s3);	
+	c = (s3*s3) == (s1*s1) + (s2*s2);
+	
+	if(a || b || c)
+		printf("Right-angled triangle\n");
+	
+	return 0;
+} 
